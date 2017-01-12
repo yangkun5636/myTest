55d89a85649106430ad5a88d7cbf4051468ce543
yangkun5636 <yangkun5636@163.com>
2017-01-12 08:01:02 +0800

    *

--- src/cn/com/index/controller/IndexAction.java
+++ src/cn/com/index/controller/IndexAction.java
@@ -4,13 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("")
 public class IndexAction {
 	
-	@RequestMapping("index")
+	@RequestMapping("/index")
 	public String index(){
 		System.out.println(1);
 		return "index";
+	}
+	
+	@RequestMapping("/butterflies")
+	public String butterflies(){
+		return "butterflies";
+	}
+	
+	@RequestMapping("/book_page")
+	public String book_page(){
+		return "book_page/book_page";
 	}
 
 }
